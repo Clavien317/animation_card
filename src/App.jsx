e87c4a2d191bea77ecda9ada6c3dcf2638dba97b
Clavien317 <Claviennambinina511@gmail.com>
2024-02-16 01:01:36 -0900

    ajout parallax

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import ThreeDCardDemo from "./components/3dCar";
+import { BackgroundGradientAnimationDemo } from "./components/Gradient";
+import { HeroParallaxDemo } from "./components/Parallax";
+// import { SparklesPreview } from "./components/SparklesPreview";
+
+/** @format */
+export default function App() {
+  return (
+    <>
+      <ThreeDCardDemo />
+      {/* <SparklesPreview /> */}
+      <HeroParallaxDemo />
+      {/* <BackgroundGradientAnimationDemo /> */}
+    </>
+  );
+}
